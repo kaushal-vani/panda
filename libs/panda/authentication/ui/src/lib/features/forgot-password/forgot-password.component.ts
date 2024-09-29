2147ac3f7770bd2fc3a2d28864d8234dda899220
kaushal2469 <kaushal2469@gmail.com>
2024-09-29 19:59:07 +0530

    update forgot-password component

---
+++ libs/panda/authentication/ui/src/lib/features/forgot-password/forgot-password.component.ts
@@ -0,0 +1,68 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ScreenAction, SCREEN_VIEW, BUTTON_NAME } from '@panda/panda/shared';
+
+// PrimeNG Modules
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
+@Component({
+  selector: 'lib-forgot-password',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    CardModule,
+  ],
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.scss'],
+})
+export class ForgotPasswordComponent implements OnInit {
+  @Output() back = new EventEmitter<ScreenAction>();
+  @Output() verify = new EventEmitter<ScreenAction>();
+  forgotPasswordForm!: FormGroup;
+  loading = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+    this.loading = true;
+    setTimeout(() => {
+      if (this.forgotPasswordForm.valid) {
+        this.verify.emit({
+          currentView: SCREEN_VIEW.FORGOT_PASSWORD,
+          buttonName: BUTTON_NAME.SUBMIT,
+          nextView: SCREEN_VIEW.VERIFICATION_COMPLETED
+        });
+        // Mock logic for password recovery (send email, etc.)
+        console.log('Submitting forgot password form:', this.forgotPasswordForm.value);
+      }
+      this.loading = false;
+    }, 2000);
+  }
+
+  navigateToLogin() {
+    this.back.emit({
+      buttonName: BUTTON_NAME.GO_TO_LOGIN,
+      currentView: SCREEN_VIEW.FORGOT_PASSWORD,
+      previousView: SCREEN_VIEW.LOGIN,
+    });
+  }
+}
