3147d031e71c3430076bd3862a6fa1b662b858cc
kaushal2469 <kaushal2469@gmail.com>
2024-09-29 20:06:25 +0530

    update index ts

---
+++ libs/panda/authentication/ui/src/index.ts
@@ -0,0 +1,14 @@
+/* Pages */
+export * from './lib/pages/authentication-page/authentication-page.component';
+
+/* Features */
+export * from './lib/features/add-email/add-email.component';
+export * from './lib/features/add-phone-number/add-phone-number.component';
+export * from './lib/features/edit-email/edit-email.component';
+export * from './lib/features/edit-phone-number/edit-phone-number.component';
+export * from './lib/features/forgot-password/forgot-password.component';
+export * from './lib/features/login/login.component';
+export * from './lib/features/one-time-password/one-time-password.component';
+export * from './lib/features/select-device/select-device.component';
+export * from './lib/features/sign-up/sign-up.component';
+export * from './lib/features/verification-completed/verification-completed.component';
---
+++ libs/panda/shared/src/index.ts
@@ -0,0 +1,10 @@
+/* Enums */
+export * from './lib/enums/button-name.enum';
+export * from './lib/enums/screen-view.enum';
+
+/* Interfaces */
+export * from './lib/models/screen-action.interfaces';
+
+/* Components */
+export * from './lib/components/header/header.component';
+export * from './lib/components/footer/footer.component';
