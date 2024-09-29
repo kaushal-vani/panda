f253d47d1b8358efd64f809aebddf689974099f6
kaushal2469 <kaushal2469@gmail.com>
2024-09-30 01:36:50 +0530

    create cookies component

---
+++ libs/panda/shared/src/lib/components/cookies/cookies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-cookies',
+  standalone: true,
+  imports: [],
+  templateUrl: './cookies.component.html',
+  styleUrl: './cookies.component.scss'
+})
+export class CookiesComponent {
+
+}
