d7792a4be71c01785797dbba3e8518fa3a6d5f9a
kaushal2469 <kaushal2469@gmail.com>
2024-09-30 01:32:54 +0530

    create page-not-found component

--- libs/panda/shared/src/index.ts
+++ libs/panda/shared/src/index.ts
@@ -8,3 +8,4 @@ export * from './lib/models/screen-action.interfaces';
 /* Components */
 export * from './lib/components/header/header.component';
 export * from './lib/components/footer/footer.component';
+export * from './lib/components/page-not-found/page-not-found.component';
