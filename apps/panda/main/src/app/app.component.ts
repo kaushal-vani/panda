2857ae4d7693cbc8b7bd69680c8ad2e58365f763
kaushal2469 <kaushal2469@gmail.com>
2024-09-29 20:08:53 +0530

    update app component

--- apps/panda/main/src/app/app.component.ts
+++ apps/panda/main/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { AuthenticationComponent } from '@panda/panda/authentication/smart';
+import { HeaderComponent } from '@panda/panda/shared';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, AuthenticationComponent, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
