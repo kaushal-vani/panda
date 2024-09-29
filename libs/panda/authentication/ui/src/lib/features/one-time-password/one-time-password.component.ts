be7738e67991916a78a5dda45e1f83cb13dfb6bf
kaushal2469 <kaushal2469@gmail.com>
2024-09-29 20:00:03 +0530

    update one-time-password component

---
+++ libs/panda/authentication/ui/src/lib/features/one-time-password/one-time-password.component.ts
@@ -0,0 +1,77 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { InputOtpModule } from 'primeng/inputotp';
+import { ScreenAction, SCREEN_VIEW, BUTTON_NAME } from '@panda/panda/shared';
+
+@Component({
+  selector: 'lib-one-time-password',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    InputOtpModule,
+    CardModule,
+  ],
+  templateUrl: './one-time-password.component.html',
+  styleUrls: ['./one-time-password.component.scss'],
+})
+export class OneTimePasswordComponent implements OnInit {
+  @Output() resend = new EventEmitter<ScreenAction>();
+  @Output() navigate = new EventEmitter<ScreenAction>();
+  @Output() verify = new EventEmitter<ScreenAction>();
+
+  value = '';
+  minutes = 5;
+  seconds = 0;
+  interval!: ReturnType<typeof setInterval>; ;
+
+  ngOnInit(): void {
+    this.startTimer();
+  }
+
+  startTimer(): void {
+    this.interval = setInterval(() => {
+      if (this.seconds > 0) {
+        this.seconds--;
+      } else {
+        if (this.minutes > 0) {
+          this.minutes--;
+          this.seconds = 59;
+        } else {
+          clearInterval(this.interval);
+          // Handle timer expiration here
+        }
+      }
+    }, 1000);
+  }
+
+  resendCode() {
+    this.resend.emit({
+      currentView: SCREEN_VIEW.ONE_TIME_PASSWORD,
+      buttonName: BUTTON_NAME.RESEND,
+    });
+  }
+
+  changeDevice() {
+    this.navigate.emit({
+      currentView: SCREEN_VIEW.ONE_TIME_PASSWORD,
+      nextView: SCREEN_VIEW.SELECT_DEVICE,
+    });
+  }
+
+  submitOTP(){
+    this.verify.emit({
+      currentView: SCREEN_VIEW.ONE_TIME_PASSWORD,
+      buttonName: BUTTON_NAME.SUBMIT
+    })
+  }
+}
