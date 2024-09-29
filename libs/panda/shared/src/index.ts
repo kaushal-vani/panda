ecaae4af89f8ef3e65b581ecb8a90a9f07bca9a5
kaushal2469 <kaushal2469@gmail.com>
2024-09-30 01:37:13 +0530

    update shared index ts

--- libs/panda/shared/src/index.ts
+++ libs/panda/shared/src/index.ts
@@ -6,6 +6,8 @@ export * from './lib/enums/screen-view.enum';
 export * from './lib/models/screen-action.interfaces';
 
 /* Components */
-export * from './lib/components/header/header.component';
+export * from './lib/components/cookies/cookies.component';
 export * from './lib/components/footer/footer.component';
+export * from './lib/components/header/header.component';
+export * from './lib/components/navigation-bot/navigation-bot.component';
 export * from './lib/components/page-not-found/page-not-found.component';
