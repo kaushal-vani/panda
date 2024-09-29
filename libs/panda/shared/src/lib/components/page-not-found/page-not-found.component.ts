fb7c4f1e9255c92753c54a0c9c9b433139c2851e
kaushal2469 <kaushal2469@gmail.com>
2024-09-30 02:53:30 +0530

    update page-not-found component

--- libs/panda/shared/src/lib/components/page-not-found/page-not-found.component.ts
+++ libs/panda/shared/src/lib/components/page-not-found/page-not-found.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'lib-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [CardModule,
+    ButtonModule,],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
 export class PageNotFoundComponent {
+  constructor(private router: Router) {}
 
+  goToHome(): void {
+    this.router.navigate(['/home']);
+  }
 }
