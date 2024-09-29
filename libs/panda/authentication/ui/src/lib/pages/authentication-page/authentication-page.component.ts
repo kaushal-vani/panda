032319a0382bcd45c712354af5473a9945facfd8
kaushal2469 <kaushal2469@gmail.com>
2024-09-29 18:33:17 +0530

    create authentication-page component

---
+++ libs/panda/authentication/ui/src/lib/pages/authentication-page/authentication-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-authentication-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './authentication-page.component.html',
+  styleUrl: './authentication-page.component.css'
+})
+export class AuthenticationPageComponent {
+
+}
