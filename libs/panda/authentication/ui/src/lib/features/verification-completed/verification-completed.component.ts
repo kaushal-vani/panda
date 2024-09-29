7fa64231fe373786503040e88b66774cbc085d40
kaushal2469 <kaushal2469@gmail.com>
2024-09-29 20:01:17 +0530

    update verification component

---
+++ libs/panda/authentication/ui/src/lib/features/verification-completed/verification-completed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-verification-completed',
+  standalone: true,
+  imports: [],
+  templateUrl: './verification-completed.component.html',
+  styleUrl: './verification-completed.component.scss'
+})
+export class VerificationCompletedComponent {
+
+}
