c79a16ff46cbc87eb9b99500ac5a86fc85997d48
kaushal2469 <kaushal2469@gmail.com>
2024-09-29 19:57:32 +0530

    update add-email component

---
+++ libs/panda/authentication/ui/src/lib/features/add-email/add-email.component.ts
@@ -0,0 +1,67 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ScreenAction, SCREEN_VIEW, BUTTON_NAME } from '@panda/panda/shared';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'lib-add-email',
+  templateUrl: './add-email.component.html',
+  styleUrls: ['./add-email.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+  ],
+})
+export class AddEmailComponent {
+  @Output() navigate = new EventEmitter<ScreenAction>();
+  @Output() back = new EventEmitter<ScreenAction>();
+
+  emailForm: FormGroup;
+  successMessage!: string;
+
+  constructor(private fb: FormBuilder) {
+    this.emailForm = this.fb.group({
+      emailAddress: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  get email() {
+    return this.emailForm.get('emailAddress');
+  }
+
+  saveEmailAddress() {
+    if (this.emailForm.valid) {
+      this.successMessage = 'Email address added successfully.';
+      setTimeout(() => {
+        this.navigate.emit({
+          currentView: SCREEN_VIEW.ADD_EMAIL,
+          buttonName: BUTTON_NAME.SAVE,
+          nextView: SCREEN_VIEW.ONE_TIME_PASSWORD,
+        });
+      }, 2000);
+    }
+  }
+
+  cancelAddEmail() {
+    this.emailForm.reset();
+    this.back.emit({
+      currentView: SCREEN_VIEW.ADD_EMAIL,
+      buttonName: BUTTON_NAME.CANCEL,
+      previousView: SCREEN_VIEW.SELECT_DEVICE,
+    });
+  }
+}
