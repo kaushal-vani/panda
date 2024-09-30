4445e288227bbbb7314346b1ee140a8674685b9b
kaushal2469 <kaushal2469@gmail.com>
2024-09-30 12:32:49 +0530

    create shipping details component

---
+++ libs/panda/check-out/ui/src/lib/features/shipping-details/shipping-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shipping-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './shipping-details.component.html',
+  styleUrl: './shipping-details.component.scss'
+})
+export class ShippingDetailsComponent {
+
+}
