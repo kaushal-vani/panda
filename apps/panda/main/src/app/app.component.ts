9ed719d455474faa55d7903fb7be5bbc504e7a59
kaushal2469 <kaushal2469@gmail.com>
2024-10-01 08:19:33 +0530

    import common module

--- apps/panda/main/src/app/app.component.ts
+++ apps/panda/main/src/app/app.component.ts
@@ -1,12 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { AuthenticationComponent } from '@panda/panda/authentication/smart';
 import { HeaderComponent } from '@panda/panda/shared';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, AuthenticationComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
