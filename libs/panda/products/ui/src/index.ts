5d54aed3c2029e87d17d3e8a53238b4cbab23478
kaushal2469 <kaushal2469@gmail.com>
2024-10-02 11:49:20 +0530

    update products ui index ts

--- libs/panda/products/ui/src/index.ts
+++ libs/panda/products/ui/src/index.ts
@@ -1,2 +1,4 @@
 /* Pages */
+export * from './lib/pages/featured-page/featured-page.component';
 export * from './lib/pages/products-page/products-page.component';
+export * from './lib/pages/sale-page/sale-page.component';
