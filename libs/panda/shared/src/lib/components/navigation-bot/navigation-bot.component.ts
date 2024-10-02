e849439bd8350b2aebf6f2c6663476cc52a7b43e
kaushal2469 <kaushal2469@gmail.com>
2024-10-02 17:39:35 +0530

    create and update navigation bot

--- libs/panda/shared/src/lib/components/navigation-bot/navigation-bot.component.ts
+++ libs/panda/shared/src/lib/components/navigation-bot/navigation-bot.component.ts
@@ -1,12 +1,66 @@
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-navigation-bot',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    DialogModule,
+    FormsModule,
+    InputTextModule,
+  ],
   templateUrl: './navigation-bot.component.html',
-  styleUrl: './navigation-bot.component.scss'
+  styleUrl: './navigation-bot.component.scss',
 })
 export class NavigationBotComponent {
+  isChatbotVisible = false;
+  userMessages: string[] = [];
+  botMessages: string[] = [];
+  initialMessage = 'initialMessage';
+  selectedOption = 'Hello! How can I assist you today?';
 
+  navigationOptions = [
+    { label: 'Home', path: '/home' },
+    { label: 'Products', path: '/products' },
+    { label: 'About-Us', path: '/about-us' },
+    { label: 'Profile', path: '/profile' },
+  ];
+
+  constructor(private router: Router) {}
+  toggleChatbot() {
+    this.isChatbotVisible = !this.isChatbotVisible;
+
+    if (this.isChatbotVisible) {
+      this.userMessages = [];
+      this.botMessages = [];
+      this.initialMessage = 'Hello! How can I assist you today?';
+    }
+  }
+  closeChatbot() {
+    this.isChatbotVisible = false;
+  }
+
+  selectOption(optionLabel: string) {
+    this.userMessages.push(optionLabel);
+    const selectedOption = this.navigationOptions.find(
+      (option) => option.label === optionLabel
+    );
+    if (selectedOption) {
+      setTimeout(() => {
+        this.botMessages.push(`Navigating to ${optionLabel}...`);
+        setTimeout(() => {
+          this.router.navigate([selectedOption.path]);
+          this.botMessages.push('Thank you for using our bot!');
+          setTimeout(() => this.closeChatbot(), 2000);
+        }, 1000);
+      }, 500);
+    }
+  }
 }
