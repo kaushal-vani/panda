7857c8614745aff72b228d467085f3b11da06163
kaushal2469 <kaushal2469@gmail.com>
2024-10-02 11:42:17 +0530

    update shipping details component

--- libs/panda/check-out/ui/src/lib/features/shipping-details/shipping-details.component.ts
+++ libs/panda/check-out/ui/src/lib/features/shipping-details/shipping-details.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'lib-shipping-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, ButtonModule],
   templateUrl: './shipping-details.component.html',
-  styleUrl: './shipping-details.component.scss'
+  styleUrl: './shipping-details.component.scss',
 })
 export class ShippingDetailsComponent {
+  @Input() shippingForm!: FormGroup;
+  @Output() next: EventEmitter<void> = new EventEmitter<void>();
+  @Output() previous: EventEmitter<void> = new EventEmitter<void>();
 
+  onNext() {
+    if (this.shippingForm.valid) {
+      this.next.emit()
+    }
+  }
+
+  onPrevious() {
+    this.previous.emit()
+  }
 }
