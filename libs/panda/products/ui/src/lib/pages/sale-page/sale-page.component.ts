a51ab6f71d61b21eb09941202d442d88e4dab579
kaushal2469 <kaushal2469@gmail.com>
2024-10-02 11:48:07 +0530

    create sale page component

---
+++ libs/panda/products/ui/src/lib/pages/sale-page/sale-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-sale-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './sale-page.component.html',
+  styleUrl: './sale-page.component.scss'
+})
+export class SalePageComponent {
+
+}
