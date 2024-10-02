48857d5800a8410030f94c8335a1372256072952
kaushal2469 <kaushal2469@gmail.com>
2024-10-02 11:48:01 +0530

    create featured page component

---
+++ libs/panda/products/ui/src/lib/pages/featured-page/featured-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-featured-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './featured-page.component.html',
+  styleUrl: './featured-page.component.scss'
+})
+export class FeaturedPageComponent {
+
+}
