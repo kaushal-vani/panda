c01db3fd5373a3e7660373269f5c361330e8c96f
kaushal2469 <kaushal2469@gmail.com>
2024-10-02 11:41:58 +0530

    update payment details component

--- libs/panda/check-out/ui/src/lib/features/payment-details/payment-details.component.ts
+++ libs/panda/check-out/ui/src/lib/features/payment-details/payment-details.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputMaskModule } from 'primeng/inputmask';
 
 @Component({
   selector: 'lib-payment-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, InputMaskModule, ButtonModule],
   templateUrl: './payment-details.component.html',
-  styleUrl: './payment-details.component.scss'
+  styleUrls: ['./payment-details.component.scss'],
 })
 export class PaymentDetailsComponent {
+  @Input() paymentForm!: FormGroup;
 
+  @Output() previous = new EventEmitter<void>();
+  @Output() finalizeCheckout = new EventEmitter<void>();
+
+  onPrevious(): void {
+    this.previous.emit(); // Emit the previous event
+  }
+
+  onComplete(): void {
+    if (this.paymentForm.valid) {
+      this.finalizeCheckout.emit(); // Emit the complete event
+    }
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const control = this.paymentForm.get(fieldName);
+    return control ? control.invalid && control.touched : false; // Explicitly handle undefined case
+  }
 }
