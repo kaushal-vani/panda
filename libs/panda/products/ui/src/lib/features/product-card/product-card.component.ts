3124dc89e7e895a64b72f02f27476c777865054e
kaushal2469 <kaushal2469@gmail.com>
2024-10-04 23:25:49 +0530

    create product card component

---
+++ libs/panda/products/ui/src/lib/features/product-card/product-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-product-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss'
+})
+export class ProductCardComponent {
+
+}
