905e4409e4c7cd08fe708eb125b4b2b8c092cea4
kaushal2469 <kaushal2469@gmail.com>
2024-10-05 15:13:13 +0530

    product card integration with product page component

--- libs/panda/products/ui/src/lib/pages/products-page/products-page.component.ts
+++ libs/panda/products/ui/src/lib/pages/products-page/products-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from '../../features/product-card/product-card.component';
+import { CommonModule } from '@angular/common';
+import { MOCK_PRODUCTS } from '../../features/product-card/mocks/product-card.mocks';
+import { Product } from '../../features/product-card/models/product-card.interfaces';
 
 @Component({
   selector: 'lib-products-page',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent, CommonModule],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.scss'
 })
 export class ProductsPageComponent {
-
+  products: Product[] = MOCK_PRODUCTS;
 }
