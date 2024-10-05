cb6dc0a2e97917ca724ac4a513ad3418c8a038d8
kaushal2469 <kaushal2469@gmail.com>
2024-10-05 15:40:53 +0530

    create cart items component

---
+++ libs/panda/shared/src/lib/components/cart-items/cart-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-cart-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-items.component.html',
+  styleUrl: './cart-items.component.scss'
+})
+export class CartItemsComponent {
+
+}
